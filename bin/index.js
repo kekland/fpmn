3be6b06b6d6e6e58339c8d2e1fbdadda1e322687
kekland <kk.erzhan@gmail.com>
2019-11-10 14:53:06 +0600

    install works now

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,15 @@
 const arg = require('arg')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk').default
 
+const pubspecUtils = require('../lib/pubspec-utils')
 const install = require('../lib/install')
 const uninstall = require('../lib/install')
 
 const commands = {
-  install: (arg) => install(arg),
-  uninstall: (arg) => uninstall(arg),
+  install,
+  uninstall,
 }
 
 const aliases = {
@@ -15,11 +19,27 @@ const aliases = {
   uninstall: commands.uninstall,
 }
 
-const cli = () => {
+const cli = async () => {
   const args = arg({})
 
+  const cwd = process.cwd()
+  console.log(cwd)
+
+  const pubspecLocation = path.join(cwd, 'pubspec.yaml')
+
+  if (!fs.existsSync(pubspecLocation)) {
+    console.error(chalk.red(`Current directory does not contain ${chalk.bold('pubspec.yaml')}.`))
+    return;
+  }
+
+  let pubspec = pubspecUtils.parsePubspec(pubspecLocation)
+
   const command = args._[0]
-  aliases[command](args._[1])
+  pubspec = await aliases[command](pubspec, args._[1])
+
+  if (pubspec) {
+    pubspecUtils.writePubspec(pubspecLocation, pubspec)
+  }
 }
 
 cli()
